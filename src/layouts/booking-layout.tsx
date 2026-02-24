8a1010fd0408a3a636144f2b8544ce045753ea86
AreYouLucky <johnmichaelcagadas@gmail.com>
2026-02-24 16:55:35 +0800

    pushed something here

--- src/layouts/booking-layout.tsx
+++ src/layouts/booking-layout.tsx
@@ -8,7 +8,7 @@ interface BookingLayoutProps {
 const BookingLayout: React.FC<BookingLayoutProps> = ({ children, className }) => {
   return (
     <div
-      className={`relative min-h-screen w-full flex lg:items-end items-center justify-center print:hidden ${className}`}
+      className={`relative min-h-screen py-10 w-full flex items-center justify-center print:hidden ${className}`}
       style={{
         backgroundImage: `url('/assets/background.jpg')`,
         backgroundSize: 'cover',
@@ -16,7 +16,12 @@ const BookingLayout: React.FC<BookingLayoutProps> = ({ children, className }) =>
       }}
     >
       <div className="absolute inset-0 bg-linear-to-b from-yellow-500/90 to-orange-500/90"></div>
-      <div className="relative z-10 flex flex-col min-h-screen">
+      <div className="relative z-10 flex flex-col min-h-screen w-full items-center">
+        <section className="w-full max-w-4xl">
+          <article className="aspect-video bg-white rounded-xl overflow-hidden flex items-center justify-center">
+            Insert advertisement video here
+          </article>
+        </section>
         <header className="flex flex-col items-center justify-center pt-12 md:pt-20 text-center">
           <img src="/assets/tc-logo.png" alt="Logo" className="h-12 md:h-22 lg:h-24 mb-4" />
           <h1 className="text-white text-xl md:text-3xl font-extrabold inter-bold uppercase">
@@ -26,9 +31,12 @@ const BookingLayout: React.FC<BookingLayoutProps> = ({ children, className }) =>
             (Mag-request ug Dokumento)
           </span>
         </header>
-        <main className="flex-1 flex justify-center items-start my-6 px-4 md:px-8 lg:px-16">
-          <div className="w-full max-w-4xl">
-            {children}
+        <main className="flex-1 w-full flex justify-center my-6 px-4 md:px-8 lg:px-16">
+          <div className="w-full max-w-4xl space-y-6">
+            <div className="w-full">
+              {children}
+            </div>
+
           </div>
         </main>
       </div>
